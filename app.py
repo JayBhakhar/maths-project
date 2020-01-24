d403ff0868dad48e3341c1e65aaf31d9790ee4fc
Jay Bhakhar <jaybhakhar@gmail.com>
2020-01-24 17:08:57 +0500

    done task2 & task3

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import itertools
 from itertools import combinations
+import matplotlib.pyplot as plt
 
 
 app = Flask(__name__)
@@ -22,23 +23,41 @@ def task1():
     return render_template('task1.html', combs=combs, number=number, r=r)
 
 
-@app.route('/task2', methods=['GET', 'POST'])
+@app.route('/task2')
 def task2():
-    if request.method == 'POST':
+    num = request.args.get('number', default='')
+    if num:
         lst = []
-        num = request.form.get('num')
-        for n in range(num):
+        ans = []
+        for n in range(int(num)):
             n = n + 1
             lst.append(n)
-
         for p in itertools.permutations(lst):
-            print(''.join(str(x) for x in p))
-    return render_template('task2.html')
+            ans.append(p)
+    else:
+        lst = []
+        ans = []
+    return render_template('task2.html',lst=lst,ans=ans)
 
 
 @app.route('/task3')
 def task3():
-    return render_template('task3.html')
+    x1 = [1, 2, 4, 5, 6]
+    y1 = [1, 4, 4, 2, 6]
+    x2 = [1, 2, 3, 4, 5, 6]
+    y2 = [4, 2, 8, 2, 4, 4]
+    def graph():
+        plt.plot(x1, y1, label="line 1", color='green', linewidth=3,
+                 marker='o', markerfacecolor='yellow', markersize=12)
+        plt.plot(x2, y2, label="line 2", color='red', linewidth=3,
+                 marker='o', markerfacecolor='blue', markersize=12)
+        plt.xlabel('x - axis')
+        plt.ylabel('y - axis')
+        plt.title('Two lines on same graph!')
+        plt.legend()
+        plt.savefig('static/images/new_plot.png')
+    graph()
+    return render_template('task3.html', url ='static/images/new_plot.png')
 
 
 @app.route('/task4')
